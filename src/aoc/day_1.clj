4e6469a5403d71c52a0f95271f27247d7ef2aab0
Tijmen Vink <tijmen@brightin.nl>
2023-12-04 20:39:19 +0100

    Solution day 1 part 2

--- src/aoc/day_1.clj
+++ src/aoc/day_1.clj
@@ -1,7 +1,11 @@
 (ns aoc.day-1
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
 
-(def NUMBERS (set (map identity "1234567890")))
+(def NUMBERS #{\1 \2 \3 \4 \5 \6 \7 \8 \9})
+(def PATTERN #"six|three|two|seven|five|eight|one|nine|four")
+(def LETTERS->DIGIT {"one" 1 "two" 2 "three" 3 "four" 4 "five" 5
+                   "six" 6 "seven" 7 "eight" 8 "nine" 9})
 
 (defn- number? [char]
   (get NUMBERS char))
@@ -15,12 +19,16 @@
       (Integer. (str (first digits) (first digits)))
       (Integer. (str (first digits) (last digits))))))
 
+(defn- letters-to-digit [s]
+  (str/replace s PATTERN (fn [match] (str (get LETTERS->DIGIT match)))))
+
 (comment
   (with-open [rdr (io/reader "resources/day_1.txt")]
     (->> (into [] (line-seq rdr))
+         (map letters-to-digit)
          (map calibration-value)
          (apply +)))
-  ;; => 53334
+  ;; => 52834
 
   ;;
   )
