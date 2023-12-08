57c53e4c56cc643348a9d9ca81030c1dbeaca5f7
Tijmen Vink <tijmen@brightin.nl>
2023-12-08 15:03:15 +0100

    Solution day 4 part 2

--- src/aoc/day_4.clj
+++ src/aoc/day_4.clj
@@ -9,19 +9,27 @@
      :card/winning-numbers (set (map #(Integer. %) (str/split (str/trim winning-numbers) #" +")))
      :card/numbers (set (map #(Integer. %) (str/split (str/trim numbers) #" +")))}))
 
-(defn- points [card]
-  (->>
-   (set/intersection (:card/winning-numbers card) (:card/numbers card))
-   (reduce (fn [points _] (if (zero? points) 1 (* 2 points))) 0)))
+(defn- matching-numbers [card]
+  (set/intersection (:card/winning-numbers card) (:card/numbers card)))
+
+(defn- collect-cards [all-cards]
+  (loop [cards all-cards
+         collected-cards 0]
+    (let [current-card (first cards)
+          new-cards (->> (range (count (matching-numbers current-card)))
+                         (map #(+ (:card/id current-card) %))
+                         (map #(nth all-cards %)))]
+      (if (seq cards)
+        (recur (concat new-cards (rest cards)) (inc collected-cards))
+        collected-cards))))
 
 (comment
   (with-open [rdr (io/reader "resources/day_4.txt")]
     (->> (line-seq rdr)
          (into [])
          (map parse-line)
-         (map points)
-         (apply +)))
-  ;; => 21821
+         (collect-cards)))
+  ;; => 5539496
 
 
   ;;
