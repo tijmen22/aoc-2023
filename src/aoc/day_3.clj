a8831101d8fb82721d0a8df7649f042731a94260
Tijmen Vink <tijmen@brightin.nl>
2023-12-08 13:38:45 +0100

    Solution day 3 part 2

--- src/aoc/day_3.clj
+++ src/aoc/day_3.clj
@@ -2,31 +2,27 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as str]))
 
-(defn- valid? [chars]
-  (when (re-find #"[^\d.]" chars) true))
+(defn- gear-position [[[ip prev] [i current] [in next]] current-number position]
+  (let [s #(subs % (max 0 (dec position)) (min (count %) (+ 1 position (count current-number))))]
+    (cond
+      (and (seq prev) (str/index-of (s prev) "*"))
+      [ip (str/index-of prev "*" (dec position))]
+      (str/index-of (s current) "*")
+      [i (str/index-of current "*" (dec position))]
+      (and (seq next) (str/index-of (s next) "*"))
+      [in (str/index-of next "*" (dec position))])))
 
 (defn- numbers [s]
   (re-seq #"\d+" s))
 
-(defn- adjacent-characters [[prev current next] number position]
-  (let [min-x (max 0 (dec position))
-        max-x (min (count current) (inc (+ position (count number))))]
-    (prn number ": " min-x position max-x)
-    (->>
-     [(if (seq prev) (subs prev min-x max-x) "")
-      (if (seq current) (subs current min-x max-x) "")
-      (if (seq next) (subs next min-x max-x) "")]
-     (str/join))))
+(defn- check-valid-numbers [[_prev [_i current] _next :as partition]]
+  (fn [r number]
+    (let [position (str/index-of current number (:last-position r))]
+      (-> (assoc r :last-position (+ position (count number)))
+          (update :numbers conj {:value (Integer. number)
+                                 :adjacent-gear (gear-position partition number position)})))))
 
-(defn- check-valid-numbers [[_prev current _next :as partition]]
-  (fn [r current-number]
-    (let [position (str/index-of current current-number (:last-position r))]
-      (cond->
-       (assoc r :last-position (+ position (count current-number)))
-        (valid? (adjacent-characters partition current-number position))
-        (update :numbers conj (Integer. current-number))))))
-
-(defn- parse-line [[_prev current _next :as partition]]
+(defn- parse-line [[_prev [_i current] _next :as partition]]
   (let [numbers (numbers current)]
     (reduce (check-valid-numbers partition) {:last-position 0} numbers)))
 
@@ -40,11 +36,16 @@
     (->> (line-seq rdr)
          (into [])
          (wrap-empty-lines)
+         (map-indexed (fn [i line] [i line]))
          (partition 3 1)
          (map parse-line)
          (mapcat :numbers)
+         (group-by :adjacent-gear)
+         (vals)
+         (filter #(= 2 (count %)))
+         (map (fn [[first second]] (* (:value first) (:value second))))
          (apply +)))
-  ;; => 537832
+  ;; => 81939900
 
 
   ;;
