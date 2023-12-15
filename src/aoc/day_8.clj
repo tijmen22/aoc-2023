769a5e3a926c5bc0b9a56d4992d0caee5bfb5cf0
Tijmen Vink <tijmen@brightin.nl>
2023-12-15 15:43:19 +0100

    Solution day 8 part 2

--- src/aoc/day_8.clj
+++ src/aoc/day_8.clj
@@ -1,13 +1,21 @@
-(ns aoc.day-8 
-  (:require
-   [clojure.java.io :as io]))
+(ns aoc.day-8
+  (:require [clojure.java.io :as io]
+            [clojure.math :as math]))
 
 (def EXAMPLE
-  ["LLR"
+  ["LR"
    ""
-   "AAA = (BBB, BBB)"
-   "BBB = (AAA, ZZZ)"
-   "ZZZ = (ZZZ, ZZZ)"])
+   "11A = (11B, XXX)"
+   "11B = (XXX, 11Z)"
+   "11Z = (11B, XXX)"
+   "22A = (22B, XXX)"
+   "22B = (22C, 22C)"
+   "22C = (22Z, 22Z)"
+   "22Z = (22B, 22B)"
+   "XXX = (XXX, XXX)"])
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Parsing
 
 (defn- parse-network [lines]
   (->> lines
@@ -23,26 +31,74 @@
   {:instructions (parse-instructions instructions)
    :network (parse-network network)})
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Helpers
+
+(defn- end-node? [node]
+  (= "Z" (subs node 2 3)))
+
+(defn- start-node? [node]
+  (= "A" (subs node 2 3)))
+
+(defn- starting-nodes [network]
+  (->> (keys network)
+       (filter start-node?)))
+
 (defn- next-node [network node instruction]
   (get-in network [node instruction]))
 
-(defn- instruction-reducer [network]
-  (fn [nodes instruction]
-    (conj nodes (next-node network (last nodes) instruction))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Reduce network
 
-(defn- reduce-instructions [starting-nodes network instructions]
-  (let [traveled-nodes (reduce (instruction-reducer network) starting-nodes instructions)]
-    (if (= "ZZZ" (last traveled-nodes))
-      traveled-nodes
-      (reduce-instructions traveled-nodes network instructions))))
+(defn- reduce-instructions [node network instructions]
+  (loop [node node
+         i 1 ;; i = 1 because all options visit 1 node before the cycle starts
+         [now & rest] instructions]
+    (let [next-node (next-node network node now)]
+      (if (end-node? next-node) i ;; all options reach their target exactly at the end of the cycle
+          (recur next-node (inc i) (or rest instructions))))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Least common multiple
+
+(defn- trial-division [n]
+  (loop [i 2]
+    (let [k (math/sqrt n)]
+      (if (<= i k)
+        (if (zero? (mod n i))
+          [i (/ n i)]
+          (recur (inc i)))
+        :prime))))
+
+(defn- factors [n]
+  (loop [numbers [n]
+         primes []]
+    (if (seq numbers)
+      (let [divided (trial-division (first numbers))]
+        (if (= :prime divided)
+          (recur (rest numbers) (conj primes (first numbers)))
+          (recur (concat (rest numbers) divided) primes)))
+      primes)))
+
+(defn- least-common-multiple [coll]
+  (->> (mapv factors coll)
+       (mapv frequencies)
+       (apply merge-with max)
+       (mapv (fn [[factor max-occurences]] (math/pow factor max-occurences)))
+       (apply *)))
 
 (comment
   (with-open [rdr (io/reader "resources/day_8.txt")]
     (let [{:keys [instructions network]} (->> (line-seq rdr) (into []) (parse-lines))]
       (->>
-       (reduce-instructions ["AAA"] network instructions)
-       (count)
-       (dec))))
+       (map #(reduce-instructions % network instructions) (starting-nodes network))
+       ;; => (11653 19241 16531 14363 19783 12737)
+       ;; (count instructions) ;; => 271
+       ;; (map #(mod % 271) '(11653 19241 16531 14363 19783 12737)) ;; => (0 0 0 0 0 0)
+       (least-common-multiple)
+       (biginteger))))
+
+  ;; => 9177460370549
 
 ;;
   )
